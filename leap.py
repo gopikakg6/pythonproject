9b2f0d64b31b25157fb0d663d50b17514e9febcd
gopika <gopikasritha1997@gmail.com>
2022-07-19 14:04:16 +0530

    first commit

---
+++ leap.py
@@ -0,0 +1,6 @@
+n=int(input('ENTER THE year:'))
+
+if n%400==0 and n%4==0:
+       print('this is leap year')
+else:
+   print('not leap year')
---
+++ project.py
@@ -0,0 +1,16 @@
+#exercise1
+# factorial=1
+# num=int(input("enter a number"))
+# if num<0:
+#     print("can't find factoril ")
+# elif num==1 or num==0:
+#     print("factorial is 1")
+# else:
+#     for i in range(1,num+1):
+#         factorial=factorial*i
+#     print(factorial)
+#.............................................
+
+#exercise 2
+#print("hello world")
+
