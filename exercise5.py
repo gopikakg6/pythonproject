337d797509bfe60a796319b74ae8a1258b592409
gopika <gopikasritha1997@gmail.com>
2022-07-20 10:55:17 +0530

    lphw exercise5 completed

---
+++ exercise5.py
@@ -0,0 +1,16 @@
+my_name='Gopika'
+my_age=25 #not a lie
+my_height=74 #inches
+my_weight=52 #lbs
+my_eyes='Black'
+my_teeth='white'
+my_hair='black'
+print(f"lets talk about {my_name}.")
+print(f"my age{my_age}.")
+print(f"my weight {my_height}.")
+print(f"my height {my_weight}.")
+print(f"my eyes are {my_eyes}.")
+print(f"my teeth color {my_teeth}.")
+print(f"my hair color{my_hair}.")
+total=my_age+my_height+my_weight
+print(f"if i add {my_height}and {my_weight} and {my_age} then get {total}")
