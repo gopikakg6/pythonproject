23bb09d2879807e09ee6ee4fa136f2639a6fcbb3
gopika <gopikasritha1997@gmail.com>
2022-07-20 10:34:41 +0530

    lphw exercise4

--- pattern.py
+++ pattern.py
@@ -5,3 +5,11 @@ for i in range(0,num):
     for j in range(0,i+1):
         print("*",end=" ")
     print()
+
+# num=int(input("enter the limit "))
+# for i in range(num,0):
+#     for j in range(0,i+1):
+#         print(end=" *")
+#     for j in range(0,i+1):
+#         print("*",end=" ")
+#     print()
